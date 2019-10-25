5928e810b270842e2510bb72c9c7e294a9103037
Jeromeski <jeromegee@gmail.com>
2019-10-25 20:15:04 +0800

    Updates

---
+++ precache-manifest.cf3f32082f7d2653e3e26540c8ef2221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1a5d4f317ade67e8b0a074390efeed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "627fc5c8e6ce2399eea2",
+    "url": "/monsters-rolodex/static/css/main.b616b745.chunk.css"
+  },
+  {
+    "revision": "8f66e7892f88ac220728",
+    "url": "/monsters-rolodex/static/js/2.e8d38444.chunk.js"
+  },
+  {
+    "revision": "627fc5c8e6ce2399eea2",
+    "url": "/monsters-rolodex/static/js/main.a88b8846.chunk.js"
+  },
+  {
+    "revision": "7f410422c000bddbff9e",
+    "url": "/monsters-rolodex/static/js/runtime-main.a7e40423.js"
+  }
+]);
